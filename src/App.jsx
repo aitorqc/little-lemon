f7a449173b21ebe36572625c4156bead0d0dd923
aquicor <aquinoacordero@gmail.com>
2023-01-08 16:04:18 +0100

    Componentes basicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <img src="" alt="" />
+            <div>
+                <ul>
+                    <li>Doormat Navigation</li>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Reservations</li>
+                    <li>Order Online</li>
+                    <li>Login</li>
+                </ul>
+                <ul>
+                    <li>Contact</li>
+                    <li>Adress</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+                <ul>
+                    <li>Social Media Links</li>
+                    <li>Adress</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
